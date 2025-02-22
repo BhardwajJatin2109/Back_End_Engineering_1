4bb462b631e5e5dc8f870e7bcd9912b1d6658968
BhardwajJatin2109 <156661099+BhardwajJatin2109@users.noreply.github.com>
2025-02-22 23:02:26 +0530

    Update app.js

--- css/app.js
+++ css/app.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
 });
 
 
-app.use(express.static('public'));
+app.use(express.static('css'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
